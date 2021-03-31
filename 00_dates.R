8eeff7975e6820d5ac0859905ef49ed174f2bdd2
Genelle Denzin <genelledenzin@cohhio.org>
2021-03-31 10:28:25 -0400

    initial commit

---
+++ 00_dates.R
@@ -0,0 +1,41 @@
+# COHHIO_HMIS
+# Copyright (C) 2021  Coalition on Homelessness and Housing in Ohio (COHHIO)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details at
+# <https://www.gnu.org/licenses/>.
+
+library(lubridate)
+library(here)
+
+# hc = hard-coded here, used elsewhere
+
+# Hard-coded Dates --------------------------------------------------------
+
+hc_bos_start_vaccine_data <- mdy("02052021")
+
+hc_psh_started_collecting_move_in_date <- mdy("10012017") 
+
+hc_began_collecting_covid_data <- mdy("04012020")
+
+hc_outreach_to_cls <- mdy("10012019")
+
+hc_began_requiring_spdats <- mdy("01012019")
+
+hc_unsheltered_data_start <- mdy("01012019")
+
+hc_prior_living_situation_required <- mdy("10012016")
+
+hc_check_eligibility_back_to <- mdy("10012016")
+
+hc_no_more_svcs_on_hh_members <- mdy("02012019")
+
+hc_first_vaccine_administered_in_us <- mdy("12142020")
+
---
+++ organizations.R
@@ -0,0 +1,90 @@
+# COHHIO_HMIS
+# Copyright (C) 2021  Coalition on Homelessness and Housing in Ohio (COHHIO)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details at
+# <https://www.gnu.org/licenses/>.
+
+library(tidyverse)
+library(lubridate)
+library(here)
+library(readxl)
+
+art_data <- read_xlsx(here("random_data/OrganizationsBitFocus.xlsx"), 
+                      sheet = 4) %>%
+  filter(!is.na(ProjectID)) %>% # dropping deleted providers
+  mutate(MinEntry = as.Date(MinEntry, origin = "1899-12-30"),
+         MaxExit = as.Date(MaxExit, origin = "1899-12-30"),
+         OpenEEs = replace_na(OpenEEs, 0),
+         operating = if_else(operating == "Yes", 1, 0),
+         participating = if_else(participating == "Yes", 1, 0))
+
+# the warnings are ok
+org_level <- art_data %>%
+  group_by(Org) %>%
+  summarise(minEntry = min(ymd(MinEntry), na.rm = TRUE),
+            maxExit = max(ymd(MaxExit), na.rm = TRUE),
+            openEEs = sum(OpenEEs),
+            maxOperating = max(operating),
+            maxParticipating = max(participating))
+
+
+# Separating Out Orgs that are coming over --------------------------------
+
+# orgs that have providers with data in them that's newer than 2014-05-01 
+# definitely coming over
+orgs_data_7yrs <- org_level %>%
+  filter(ymd(maxExit) > ymd("20140501")) %>%
+  select(Org) %>%
+  unique()
+
+# orgs where the most recent data entered is before 2014-05-01
+# definitely not coming over
+orgs_data_old <- org_level %>%
+  filter(ymd(maxExit) <= ymd("20140501")) %>%
+  select(Org) %>%
+  unique() 
+
+# Shell or Not-Shell ------------------------------------------------------
+
+# some provider in the org is HMIS participating
+# definitely coming over, all not-shell
+orgs_data_7yrs_participating <- orgs_data_7yrs %>%
+  left_join(org_level, by = "Org") %>%
+  filter(maxParticipating == 1)
+
+
+
+# no providers in the org are HMIS participating
+# maybe coming over
+orgs_data_7yrs_not_participating <- orgs_data_7yrs %>%
+  left_join(org_level, by = "Org") %>%
+  filter(maxParticipating == 0)
+
+
+
+VASH_only_orgs <- art_data %>%
+  mutate(VASH = if_else(str_detect(Project, "VASH"), 1, 0)) %>%
+  group_by(Org) %>%
+  summarise(total = n(),
+            vash = sum(VASH)) %>%
+  ungroup() %>%
+  filter(total - vash == 0) %>%
+  select(Org)
+
+non_participating_orgs <- art_data %>%
+  anti_join(orgs_data_7yrs, by = "Org") %>%
+  filter(str_starts(Project, "zz", negate = TRUE) &
+           participating == "No") %>%
+  # select(Org) %>%
+  unique()
+
+org_same_as_project <- art_data %>%
+  filter(Org == Project)

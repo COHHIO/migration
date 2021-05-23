4057658b9a9f2df1dcac6357808e1e5462694574
Genelle Denzin <genelledenzin@cohhio.org>
2021-05-23 14:58:55 -0400

    gets all severance csvs into dataframes

---
+++ reading_severance.R
@@ -0,0 +1,29 @@
+# read severance file from WS
+
+library(data.table)
+library(here)
+library(tidyverse)
+
+
+# Get all csv files into dfs ----------------------------------------------
+
+files <- list.files(path = "random_data/initial_severance/", pattern = ".csv")
+
+make_dfs <- function(file) {
+  cat(file, sep = "\n")
+  x <-
+    read_csv(here(paste0("random_data/initial_severance/", file)),
+    col_types = cols())
+  
+  x <- data.frame(x)
+  
+  assign(str_remove(file, ".csv"),
+         x,
+         envir = .GlobalEnv)
+  
+}
+
+data.frame(file = files) %>%
+  purrr::pwalk(make_dfs)
+
+

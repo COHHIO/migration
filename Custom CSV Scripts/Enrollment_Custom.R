eb100b282a5c2cb845336de2a1eb888e29551679
Genelle Denzin <genelledenzin@cohhio.org>
2021-05-25 11:41:30 -0400

    The severance file gives a group_id instead of an entry_exit_id in the entry_exit_answer_link csv so I'm not able to connect answers to entry exits using the severance file. This forced me to have to use an ART report. Losing a lot of nuance, but whatever, we gotta move it along.

--- Custom CSV Scripts/Enrollment_Custom.R
+++ Custom CSV Scripts/Enrollment_Custom.R
@@ -17,79 +17,30 @@ library(tidyverse)
 library(readxl)
 library(lubridate)
 
-if(!exists("da_answer")) {
-  source(here("reading_severance.R"))
+from_ART1 <- read_xlsx("random_data/Enrollment_Custom_ART.xlsx", sheet = 1) %>%
+  mutate(InformationDate = as.Date(InformationDate, origin = "1899-12-30"))
+
+from_ART2 <- read_xlsx("random_data/Enrollment_Custom_ART.xlsx", sheet = 2) %>%
+  mutate(InformationDate = as.Date(InformationDate, origin = "1899-12-30"))
+
+from_ART <- rbind(from_ART1, from_ART2) %>%
+  mutate(ExportID = as.numeric(today()))
+
+# Writing it out to csv ---------------------------------------------------
+
+write_csv(from_ART, here("data_to_Clarity/Enrollment_Custom.csv"))
+
+fix_date_times <- function(file) {
+  cat(file, sep = "\n")
+  x <- read_csv(here(paste0("data_to_Clarity/", file, ".csv")),
+                col_types = cols())  %>%
+    mutate(
+      InformationDate = format.Date(InformationDate, "%Y-%m-%d")
+    )
+  
+  fwrite(x,
+         here(paste0("data_to_Clarity/", file, ".csv")),
+         logical01 = TRUE)
 }
 
-
-# get ees together --------------------------------------------------------
-
-ees <- sp_entry_exit %>%
-  left_join(entry_exit_answer_link, by = "entry_exit_id") %>%
-  select(entry_exit_id, client_id, answer_id, point_in_time_type)
-
-# County Served -----------------------------------------------------------
-
-county_served <- da_answer %>% 
-  filter(question_code == "COUNTY" & active == TRUE) %>%
-  rename("date_answer_added" = date_added) %>%
-  left_join(ees, by = c("client_id", "answer_id")) %>%
-  filter(!is.na(entry_exit_id)) %>%
-  select("EnrollmentID" = entry_exit_id,
-         "PersonalID" = client_id,
-         "InformationDate" = date_effective,
-         "DataCollectionStage" = point_in_time_type,
-         "DateUpdate" = date_added,
-         "UserID" = user_id,
-         "county_served" = val
-         ) %>%
-  mutate(DataCollectionStage = case_when(
-    DataCollectionStage == "Entry" ~ 1,
-    DataCollectionStage == "Interim Review" ~ 2,
-    DataCollectionStage == "Exit" ~ 3
-  ))
-
-county_prior <- da_answer %>% 
-  filter(question_code == "COUNTYOFRESIDENCEPRIOR" & active == TRUE) %>%
-  rename("date_answer_added" = date_added) %>%
-  left_join(ees, by = c("client_id", "answer_id")) %>%
-  filter(!is.na(entry_exit_id)) %>%
-  select("EnrollmentID" = entry_exit_id,
-         "PersonalID" = client_id,
-         "InformationDate" = date_effective,
-         "DataCollectionStage" = point_in_time_type,
-         "DateUpdate" = date_added,
-         "UserID" = user_id,
-         "county_prior" = val
-  ) %>%
-  mutate(DataCollectionStage = case_when(
-    DataCollectionStage == "Entry" ~ 1,
-    DataCollectionStage == "Interim Review" ~ 2,
-    DataCollectionStage == "Exit" ~ 3
-  ))
-
-Enrollment_Custom <- county_served %>%
-  full_join(county_prior, by = c("EnrollmentID",
-                                 "PersonalID",
-                                 "InformationDate",
-                                 "DataCollectionStage",
-                                 "DateUpdate",
-                                 "UserID")) %>%
-  mutate(ExportID = as.numeric(today()),
-         EnrollmentCustomID = row_number()) %>%
-  relocate(EnrollmentCustomID, .before = EnrollmentID) %>%
-  relocate(ExportID, .after = UserID)
-
-
-# Checks ------------------------------------------------------------------
-
-art_counties <- read_xlsx("data_to_Clarity/RMisc2.xlsx", sheet = 1) %>% 
-  select(EnrollmentID, CountyPrior, CountyServed) %>%
-  filter(!is.na(CountyPrior) | !is.na(CountyServed))
-
-severance_counties <- Enrollment_Custom %>%
-  select(EnrollmentID, county_prior, county_served)
-
-mutual_ee_ids <- art_counties %>%
-  left_join(severance_counties, by = "EnrollmentID")
-
+fix_date_times("Enrollment_Custom")

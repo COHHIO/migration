48901f8a18bd9343fce2efeae85a352741418a00
Genelle Denzin <genelledenzin@cohhio.org>
2021-05-26 17:03:31 -0400

    getting all the notes in SP together (not done)

--- Custom CSV Scripts/Notes_Custom.R
+++ Custom CSV Scripts/Notes_Custom.R
@@ -19,7 +19,62 @@ library(lubridate)
 
 source(here("reading_severance.R"))
 
+# Notes attached to Goals -------------------------------------------------
 
-# look in da_recordset and da_recordset_answer
+raw_notes_goals <- read_xlsx("random_data/Notes.xlsx", sheet = 1)
+
+notes_goals <- raw_notes_goals %>%
+  mutate(Title = "imported from Goals",
+         Program = str_extract(Program, "[0-9]+"),
+         UserID = str_extract(UserID, "[0-9]+"))
+
+# Client Notes ------------------------------------------------------------
+
+raw_notes_client <- read_xlsx("random_data/Notes.xlsx", sheet = 2)
+
+notes_client <- raw_notes_client %>%
+  mutate(Title = "imported from Clients",
+         Program = str_extract(Program, "[0-9]+"),
+         UserID = str_extract(UserID, "[0-9]+"))
+
+# Notes attached to Services ----------------------------------------------
+
+service_notes <- sp_need_service %>% 
+  filter(!is.na(service_note) & active == TRUE) %>% 
+  select(client_id, date_added, provide_provider_id, service_note) %>%
+  mutate(Title = "imported from ServicePoint Service")
+
+# Notes attached to Needs -------------------------------------------------
+
+need_notes <- sp_need %>% 
+  filter(!is.na(note) & active == TRUE) %>% 
+  select(client_id, date_added, provider_id, note) %>%
+  mutate(Title = "imported from ServicePoint Need")
+
+# Exit Notes --------------------------------------------------------------
+
+exit_notes <- sp_entry_exit %>%
+  filter(!is.na(notes) & active == TRUE) %>%
+  select(client_id, 
+         date_added, 
+         date_updated, 
+         entry_exit_id, 
+         exit_date,
+         provider_id, 
+         notes) %>%
+  mutate(Title = paste("Exit note: Exited Project ID", 
+                       provider_id, 
+                       "on", 
+                       format.Date(exit_date, "%m-%d-%Y")))
+
+
+
+
+
+
+
+
+
+
 
 

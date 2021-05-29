455673f77acaf01d123652a9b369e7d7ccf87ebd
Genelle Denzin <genelledenzin@cohhio.org>
2021-05-29 16:35:46 -0400

    add in AP data manually using this output

--- manual_entry_helpers/Access_Points.R
+++ manual_entry_helpers/Access_Points.R
@@ -23,28 +23,36 @@ Access_Points <- sp_provider %>%
 AP_General <- sp_provider_service_geography_served_by_county %>%
   filter(provider_id %in% c(Access_Points)) %>%
   group_by(provider_id) %>%
-  summarise(counties = toString(county))
+  summarise(counties = toString(county)) %>%
+  ungroup() %>%
+  rename("general_service_area" = counties)
 
 AP_Veteran <- sp_provider_service_code %>% 
   filter(provider_id %in% c(Access_Points) & 
            service_code == "FT-1000.9000") %>% 
-  select(provider_id, service_area) 
+  select(provider_id, "veteran_service_area" = service_area) 
 
 AP_Youth <- sp_provider_service_code %>% 
   filter(provider_id %in% c(Access_Points) & 
            service_code == "RP-1400.8000-750") %>% 
-  select(provider_id, service_area)
+  select(provider_id, "youth_service_area" = service_area)
 
 AP_DV <- sp_provider_service_code %>% 
   filter(provider_id %in% c(Access_Points) & 
            service_code == "BH-1800.1500-100") %>% 
-  select(provider_id, service_area)
+  select(provider_id, "dv_service_area" = service_area)
 
 hours_website <- sp_provider %>%
   filter(provider_id %in% c(Access_Points)) %>%
   select(provider_id, hours, website_address)
 
+access_point_manual <- AP_General %>%
+  full_join(AP_Veteran, by = "provider_id") %>%
+  full_join(AP_Youth, by = "provider_id") %>%
+  full_join(AP_DV, by = "provider_id") %>%
+  full_join(hours_website, by = "provider_id")
 
+write_csv(access_point_manual, "random_data/ap_manual_data_entry.csv")
 
 
 

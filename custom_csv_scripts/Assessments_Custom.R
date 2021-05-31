2ead42b8a385c616a8381e93ef424ec4ea3a132e
Genelle Denzin <genelledenzin@cohhio.org>
2021-05-31 11:08:03 -0400

    starting Assessments_Custom

---
+++ custom_csv_scripts/Assessments_Custom.R
@@ -0,0 +1,22 @@
+# COHHIO_HMIS
+# Copyright (C) 2021  Coalition on Homelessness and Housing in Ohio (COHHIO)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details at
+# <https://www.gnu.org/licenses/>.
+
+library(here)
+library(tidyverse)
+library(readxl)
+library(lubridate)
+
+source(here("reading_severance.R"))
+
+

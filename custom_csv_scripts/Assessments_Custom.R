5e7f0e0b72760c29211552baa64b1fc9b9916c8a
Genelle Denzin <genelledenzin@cohhio.org>
2021-05-31 12:30:49 -0400

    fighting with pivot_wider()

--- custom_csv_scripts/Assessments_Custom.R
+++ custom_csv_scripts/Assessments_Custom.R
@@ -20,3 +20,55 @@ library(lubridate)
 source(here("reading_severance.R"))
 
 
+what_are_the_questions <- da_recordset %>%
+  filter(question %in% c("VI-SPDAT v2.0", "VI-FSPDAT v2.0", "TAY-VI-SPDAT v1.0")) %>%
+  rename("subassessment_name" = question) %>%
+  left_join(da_recordset_answer, by = "recordset_id") %>%
+  rename("question_name" = question) %>%
+  select(subassessment_name, question_name) %>% unique() %>%
+  filter(
+    !question_name %in% c(
+      "PRE-SURVEY",
+      "A. HISTORY OF HOUSING AND HOMELESSNESS",
+      "B. RISKS",
+      "C. SOCIALIZATION & DAILY FUNCTIONS",
+      "D. WELLNESS",
+      "E. FAMILY UNIT",
+      "GRAND TOTAL"
+    )
+  )
+
+
+fspdat_data <- da_recordset %>%
+  filter(question == "VI-FSPDAT v2.0" & active == TRUE) %>%
+  rename("subassessment_name" = question) %>%
+  left_join(da_recordset_answer %>%
+              filter(active == TRUE), by = "recordset_id") %>%
+  rename("question_name" = question) %>%
+  filter(
+    !question_name %in% c(
+      "PRE-SURVEY",
+      "A. HISTORY OF HOUSING AND HOMELESSNESS",
+      "B. RISKS",
+      "C. SOCIALIZATION & DAILY FUNCTIONS",
+      "D. WELLNESS",
+      "E. FAMILY UNIT",
+      "GRAND TOTAL"
+    )
+  ) %>%
+  select("PersonalID" = client_id,
+         "provider_creating" = provider_creating_id.x, 
+         "InformationDate" = date_effective,
+         question_name, val) %>%
+  pivot_wider(id_cols = c(PersonalID, provider_creating, InformationDate), 
+              names_from = question_name, 
+              values_from = val) %>%  
+  mutate(
+    AssessmentID = "87",
+    AssessmentName = "VI-F-SPDAT Prescreen for Families [V2]",
+    InformationDate = format.Date(InformationDate, "%Y-%m-%d")
+  )
+
+
+
+

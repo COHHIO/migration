81f181cb7f3b3a054353ef5537a1c80ce9e8f970
Genelle Denzin <genelledenzin@cohhio.org>
2021-06-04 17:25:14 -0400

    responding to DAR

--- custom_csv_scripts/Client_Custom.R
+++ custom_csv_scripts/Client_Custom.R
@@ -31,6 +31,39 @@ sp_question_codes <- c(
   "DATEVETERANIDENTIFIED"
 )
 
+answers_ph_track <- tibble(
+  ReferenceNo = c(0:6),
+  Value = c("None", "HUD - VASH", "PH", "PSH", "RRH", "Self Resolve", "SSVF-RRH")
+)
+
+answers_enhanced_yes_no <- tibble(
+  ReferenceNo = c(0, 1, 8, 9, 99),
+  Value = c("No (HUD)", "Yes (HUD)", "Client doesn't know (HUD)", 
+            "Client refused (HUD)", "Data not collected (HUD)")
+)
+
+answers_list_status <- tibble(
+  ReferenceNo = c(1:5),
+  Value = c("Active - ES/TH",
+            "Active - Unsheltered",
+            "Inactive (Non-Permanent Housing)",
+            "Inactive (Permanently Housed)",
+            "Inactive (Uknown/Missing)")
+)
+
+answers_ssvf_ineligible <- tibble(
+  ReferenceNo = c(1:3),
+  Value = c("Max TFA", "Not VA eligible", "Over income")
+)
+
+answers_va_eligible <- tibble(
+  ReferenceNo = c(1:4),
+  Value = c("VA eligibility unknown",
+            "Veteran eligible for all VA homeless services",
+            "Veteran eligible for SSVF/GPD only",
+            "Veteran not eligible for VA services")
+)
+
 client_level <- da_answer %>%
   filter(question_code %in% c(sp_question_codes) &
            active == TRUE) %>%
@@ -51,22 +84,39 @@ client_level <- da_answer %>%
   
   rename(
     "PersonalID" = client_id,
-    "ph_track" = PERMANENTHOUSINGTRACK,
-    "expected_ph_date" =	EXPECTEDPERMANENTHOUS,
-    "covid19_consent_to_vaccine" =	WOULDTHECLIENTCONSENT,
-    "covid19_vaccine_concerns" =	IFNOTWHATARETHECONCER,
-    "homes_id" =	HOMESID,
-    "list_status" =	LISTSTATUS,
-    "ssvf_ineligible" =	SSVFINELIGIBLE,
-    "va_eligible" =	VAELIGIBLE,
-    "date_veteran_identified" =	DATEVETERANIDENTIFIED
+    "c_ph_track" = PERMANENTHOUSINGTRACK,
+    "c_expected_ph_date" =	EXPECTEDPERMANENTHOUS,
+    "c_covid19_consent_to_vaccine" =	WOULDTHECLIENTCONSENT,
+    "c_covid19_vaccine_concerns" =	IFNOTWHATARETHECONCER,
+    "c_homes_id" =	HOMESID,
+    "c_list_status" =	LISTSTATUS,
+    "c_ssvf_ineligible" =	SSVFINELIGIBLE,
+    "c_va_eligible" =	VAELIGIBLE,
+    "c_date_veteran_identified" =	DATEVETERANIDENTIFIED
   ) %>%
   relocate(ClientCustomID, .before = "PersonalID") %>%
   relocate(ExportID, .after = "PersonalID")
 
+client_custom <- client_level %>%
+  left_join(answers_enhanced_yes_no, by = c("c_covid19_consent_to_vaccine" = "Value")) %>%
+  mutate(c_covid19_consent_to_vaccine = ReferenceNo,
+         ReferenceNo = NULL) %>%
+  left_join(answers_list_status, by = c("c_list_status" = "Value")) %>%
+  mutate(c_list_status = ReferenceNo,
+         ReferenceNo = NULL)  %>%
+  left_join(answers_ph_track, by = c("c_ph_track" = "Value")) %>%
+  mutate(c_ph_track = ReferenceNo,
+         ReferenceNo = NULL)  %>%
+  left_join(answers_ssvf_ineligible, by = c("c_ssvf_ineligible" = "Value")) %>%
+  mutate(c_ssvf_ineligible = ReferenceNo,
+         ReferenceNo = NULL)  %>%
+  left_join(answers_va_eligible, by = c("c_va_eligible" = "Value")) %>%
+  mutate(c_va_eligible = ReferenceNo,
+         ReferenceNo = NULL) 
+
 # Writing it out to csv ---------------------------------------------------
 
-write_csv(client_level, here("data_to_Clarity/Client_Custom.csv"))
+write_csv(client_custom, here("data_to_Clarity/Client_Custom.csv"))
 
 fix_date_times <- function(file) {
   cat(file, sep = "\n")

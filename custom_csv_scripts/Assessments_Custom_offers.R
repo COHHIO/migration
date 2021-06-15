bd4258d984700bad5d092ca3461dc4a654a7e4c4
Genelle Denzin <genelledenzin@cohhio.org>
2021-06-15 17:36:54 -0400

    broke out offers code to see if it needed the same transformations that the vispdat data did (and I don't think it does, but leaving the code separate makes sense for now)

---
+++ custom_csv_scripts/Assessments_Custom_offers.R
@@ -0,0 +1,142 @@
+# COHHIO_HMIS
+# Copyright (C) 2021  Coalition on Homelessness and Housing in Ohio (COHHIO)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details at
+# <https://www.gnu.org/licenses/>.
+
+library(here)
+library(tidyverse)
+library(readxl)
+library(lubridate)
+library(janitor)
+
+source(here("reading_severance.R"))
+
+
+# Getting filtered data sets ----------------------------------------------
+
+offer_subs <- da_recordset %>%
+  filter(active == TRUE & question == "Offers of Permanent Housing") %>% 
+  rename("subassessment_name" = question,
+         "sub_is_active" = active,
+         "sub_date_added" = date_added,
+         "sub_provider_created" = provider_creating_id,
+         "sub_user_created" = user_creating_id) 
+
+offer_answers <- da_recordset_answer %>%
+  filter(
+    active == TRUE &
+      question %in% c(
+        "Date of accept or decline",
+        "Date of PH Intervention Offer",
+        "Offer Accepted?",
+        "Type of PH Intervention Offered"
+      )
+  ) %>% 
+  rename(
+    "question_name" = question,
+    "answer_active" = active,
+    "answer_date_added" = date_added,
+    "answer_provider_created" = provider_creating_id,
+    "answer_user_created" = user_creating_id
+  ) %>%
+  semi_join(offer_subs,
+            by = "recordset_id")
+
+# Deduplicating subs and then answers based on correct date_effective -----
+
+deduplicated <- offer_subs %>%
+  left_join(offer_answers, by = "recordset_id") %>%
+  group_by(recordset_id, question_name) %>%
+  slice_max(answer_date_added) %>% 
+  ungroup() 
+
+# Translators -------------------------------------------------------------
+
+
+
+# Projects to Organizations -----------------------------------------------
+
+projects_orgs <- read_xlsx(
+  here("data_to_Clarity/RMisc2.xlsx"),
+  sheet = 3,
+  col_types = c("numeric", replicate(16, "text"))
+) %>%
+  filter(!is.na(OrganizationName) &
+           OrganizationName != "Coalition on Homelessness and Housing in Ohio(1)") %>%
+  mutate(
+    AgencyID = str_extract(OrganizationName, "\\(?[0-9]+\\)?"),
+    AgencyID = str_remove(AgencyID, "[(]"),
+    AgencyID = str_remove(AgencyID, "[)]"),
+    AgencyID = as.numeric(AgencyID),
+    AgencyName = str_remove(OrganizationName, "\\(.*\\)")
+  ) %>%
+  select(ProjectID, ProjectName, AgencyID, AgencyName)
+
+
+# building vi-spdat -------------------------------------------------------
+
+agencies <- read_csv("frozen/Agencies.csv") %>% pull(id)
+
+offer_data <- deduplicated %>%
+  select("PersonalID" = client_id,
+         "xProjectIDx" = sub_provider_created, # waiting on new severance file
+         "InformationDate" = date_effective,
+         offer_accept_decline_date,
+         offer_date,
+         offer_accepted,
+         offer_type
+  ) %>%
+  mutate(
+    AssessmentID = 194,
+    AssessmentName = "Offers of Permanent Housing",
+    InformationDate = format.Date(InformationDate, "%Y-%m-%d"),
+    offer_accepted = recode(
+      offer_accepted,
+      'answer 1' = 1,
+      'answer 2' = 2,
+      'answer 3' = 3
+    ),
+    offer_type = recode(
+      offer_type,
+      'answer 1' = 1
+    )
+  ) %>%
+  left_join(projects_orgs, by = c("xProjectIDx" = "ProjectID")) %>%
+  mutate(AgencyID = case_when(xProjectIDx %in% c(2372, 1695) ~ xProjectIDx,
+                              TRUE ~ AgencyID),
+         AssessmentCustomID = row_number()) %>% 
+  filter(!is.na(ProjectName) & (AgencyID %in% c(agencies) |
+                                  AgencyID == 2372)) %>%
+  select(AssessmentCustomID,
+         AssessmentID,
+         AssessmentName,
+         PersonalID,
+         AgencyID,
+         InformationDate,
+         assessment_date,
+         assessment_type,
+         assessment_level,
+         assessment_location,
+         c_vispdat_type,
+         c_vispdat_program_name,
+         c_vispdat_score) 
+
+# Offers ------------------------------------------------------------------
+
+offers_subs <- da_recordset %>%
+  filter(question == "Offers of Permanent Housing" & active == TRUE)
+
+offers_answers <- da_recordset_answer %>%
+  filter(active == TRUE) %>%
+  semi_join(offers_subs, by = "recordset_id") %>%
+  pivot_wider(names_from = question, values_from = val)
+

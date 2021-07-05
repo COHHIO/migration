126e97f1bd826153cd52410439040560cf30d040
Genelle Denzin <genelledenzin@cohhio.org>
2021-07-05 19:13:14 -0400

    tightening up code, losing nulls, fixing granularity issues, adding helper columns

--- manual_entry_helpers/Access_Points.R
+++ manual_entry_helpers/Access_Points.R
@@ -13,6 +13,7 @@
 # <https://www.gnu.org/licenses/>.
 
 library(tidyverse)
+library(here)
 
 source(here("reading_severance.R"))
 
@@ -21,7 +22,9 @@ Access_Points <- sp_provider %>%
   pull(provider_id)
 
 AP_General <- sp_provider_service_geography_served_by_county %>%
+  select(-provider_service_code_id) %>%
   filter(provider_id %in% c(Access_Points)) %>%
+  unique() %>%
   group_by(provider_id) %>%
   summarise(counties = toString(county)) %>%
   ungroup() %>%
@@ -34,23 +37,34 @@ AP_Veteran <- sp_provider_service_code %>%
 
 AP_Youth <- sp_provider_service_code %>% 
   filter(provider_id %in% c(Access_Points) & 
-           service_code == "RP-1400.8000-750") %>% 
+           service_code == "RP-1400.8000-750" &
+           !is.na(service_area)) %>% 
   select(provider_id, "youth_service_area" = service_area)
 
 AP_DV <- sp_provider_service_code %>% 
   filter(provider_id %in% c(Access_Points) & 
-           service_code == "BH-1800.1500-100") %>% 
+           service_code == "BH-1800.1500-100" &
+           !is.na(service_area)) %>% 
   select(provider_id, "dv_service_area" = service_area)
 
 hours_website <- sp_provider %>%
-  filter(provider_id %in% c(Access_Points)) %>%
-  select(provider_id, hours, website_address)
+  filter(provider_id %in% c(Access_Points) &
+           (!is.na(hours) |
+              !is.na(website_address))) %>%
+  select(provider_id, hours, website_address) 
 
 access_point_manual <- AP_General %>%
   full_join(AP_Veteran, by = "provider_id") %>%
   full_join(AP_Youth, by = "provider_id") %>%
   full_join(AP_DV, by = "provider_id") %>%
-  full_join(hours_website, by = "provider_id")
+  full_join(hours_website, by = "provider_id") %>%
+  left_join(sp_provider %>%
+              select(provider_id, name), by = "provider_id") %>%
+  left_join(id_cross %>%
+              select(Legacy_ProgramID, Clarity_AgencyName, Clarity_ProgramName), 
+            by = c("provider_id" = "Legacy_ProgramID")) %>%
+  relocate(c(name:Clarity_ProgramName), .before = provider_id) %>%
+  rename("SP_ProjectName" = name, "SP_ProjectID" = provider_id)
 
 write_csv(access_point_manual, "random_data/ap_manual_data_entry.csv")
 

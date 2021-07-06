7b399191c77badf64810abb879ee50cda5f37967
Genelle Denzin <genelledenzin@cohhio.org>
2021-07-06 17:24:37 -0400

    trying to compare mig export to sp export

--- review_scripts/compare_client_data_mig_sp.R
+++ review_scripts/compare_client_data_mig_sp.R
@@ -21,24 +21,24 @@ library(janitor)
 
 # ServicePoint ------------------------------------------------------------
 
-directory <- "data_to_Clarity"
+sp_directory <- "data_to_Clarity/initial_csv"
 
-if(ncol(read_csv(paste0(directory, "/Client.csv"))) == 36) {
-  Client <-
-    read_csv(paste0(directory, "/Client.csv"),
+if(ncol(read_csv(paste0(sp_directory, "/Client.csv"))) == 36) {
+  SP_Client <-
+    read_csv(paste0(sp_directory, "/Client.csv"),
              col_types = "nccccncnDnnnnnnnnnnnnnnnnnnnnnnTTcTn") %>%
     filter(!PersonalID %in% c(5, 4216)) # our fake Client IDs are 5 and 4216
 } else {
-  Client <-
-    read_csv(paste0(directory, "/Client.csv"),
+  SP_Client <-
+    read_csv(paste0(sp_directory, "/Client.csv"),
              col_types = "ncncnDnnnnnnnnnnnnnnnnnnnnnnTTcTn") %>%
     filter(!PersonalID %in% c(5, 4216))
 }
 
 # Masking PII in the Client file (but not DOB) 
 
-if(ncol(read_csv(paste0(directory, "/Client.csv"))) == 36)
-{Client <- Client %>%
+if(ncol(read_csv(paste0(sp_directory, "/Client.csv"))) == 36)
+{SP_Client <- SP_Client %>%
   mutate(
     # FirstName = case_when(
     #   NameDataQuality %in% c(8, 9) ~ "DKR",
@@ -78,7 +78,7 @@ if(ncol(read_csv(paste0(directory, "/Client.csv"))) == 36)
     )
   )
 
-Client <- Client %>%
+SP_Client <- SP_Client %>%
   mutate(SSN = case_when(
     is.na(SSN) ~ "ok",
     !is.na(SSN) ~ SSN
@@ -87,61 +87,64 @@ Client <- Client %>%
 # this overwrites the raw Client.csv file on your computer with the final Client
 # object as a security measure.
 
-if(ncol(Client) == 33)
-{write_csv(Client, paste0(directory, "/Client.csv"), append = FALSE)}
+if(ncol(SP_Client) == 33)
+{write_csv(SP_Client, paste0(sp_directory, "/Client.csv"), append = FALSE)}
 
 # CurrentLivingSituation <- 
-#   read_csv(paste0(directory, "/CurrentLivingSituation.csv"),
+#   read_csv(paste0(sp_directory, "/CurrentLivingSituation.csv"),
 #             col_types = "nnnTncnnnnncTTcTc") DON'T NEED YET
 
-Disabilities <-
-  read_csv(paste0(directory, "/Disabilities.csv"),
+SP_Disabilities <-
+  read_csv(paste0(sp_directory, "/Disabilities.csv"),
            col_types = "cnnDnnnnnnnnnnTTnTn")
 
-EmploymentEducation <-
-  read_csv(paste0(directory, "/EmploymentEducation.csv"),
+SP_EmploymentEducation <-
+  read_csv(paste0(sp_directory, "/EmploymentEducation.csv"),
            col_types = "cnnDnnnnnnTTnTn")
 
-Exit <-
-  read_csv(paste0(directory, "/Exit.csv"),
+SP_Exit <-
+  read_csv(paste0(sp_directory, "/Exit.csv"),
            col_types = "nnnDncnnnnnnnnnnnnnnnnnnnnnnnnnDnnnnnnTTnTn")
 
-Project <- 
-  read_csv(paste0(directory, "/Project.csv"),
+SP_Export <- read_csv(paste0(sp_directory, "/Export.csv"),
+                      col_types = c("iicccccccTDDcciii"))
+
+SP_Project <- 
+  read_csv(paste0(sp_directory, "/Project.csv"),
            col_types = "nnccDDnnnnnnnnTTcTn") 
 
-EnrollmentCoC <- 
-  read_csv(paste0(directory, "/EnrollmentCoC.csv"), 
+SP_EnrollmentCoC <- 
+  read_csv(paste0(sp_directory, "/EnrollmentCoC.csv"), 
            col_types = "cncnnDcnTTnTn")
 
-Enrollment <-
-  read_csv(paste0(directory, "/Enrollment.csv"),
+SP_Enrollment <-
+  read_csv(paste0(sp_directory, "/Enrollment.csv"),
            col_types =
              "nnnDcnnnlnDnnnDDDnnnncccnnDnnnncnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnTTnTn")
 
-Funder <- 
-  read_csv(paste0(directory, "/Funder.csv"),
+SP_Funder <- 
+  read_csv(paste0(sp_directory, "/Funder.csv"),
            col_types = "nnnccDDTTcTn")
 
-HealthAndDV <-
-  read_csv(paste0(directory, "/HealthAndDV.csv"),
+SP_HealthAndDV <-
+  read_csv(paste0(sp_directory, "/HealthAndDV.csv"),
            col_types = "cnnDnnnnnnnDnTTnTn")
 
-IncomeBenefits <- 
-  read_csv(paste0(directory, "/IncomeBenefits.csv"),
+SP_IncomeBenefits <- 
+  read_csv(paste0(sp_directory, "/IncomeBenefits.csv"),
            col_types = 
              "cnnDnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnncnnnnnnncnnnnnnnnnnnnnnnnnnnncnnnnnnTTnTn")
 
-Inventory <-
-  read_csv(paste0(directory, "/Inventory.csv"),
+SP_Inventory <-
+  read_csv(paste0(sp_directory, "/Inventory.csv"),
            col_types = "nncnnnnnnnnnnnnDDTTcTn")
 
-Organization <- 
-  read_csv(paste0(directory, "/Organization.csv"),
+SP_Organization <- 
+  read_csv(paste0(sp_directory, "/Organization.csv"),
            col_types = "ncncTTnTn")
 
-ProjectCoC <- 
-  read_csv(paste0(directory, "/ProjectCoC.csv"),
+SP_ProjectCoC <- 
+  read_csv(paste0(sp_directory, "/ProjectCoC.csv"),
            col_types = "nncnccccnnTTcTn")
 
 
@@ -152,10 +155,136 @@ clarity_enrollment_count <-
   select("ProjectName" = 1, "clarity_count" = 2) %>%
   mutate(clarity_count = as.integer(clarity_count))
 
+clarity_directory <- "data_from_Clarity"
+
+if(ncol(read_csv(paste0(clarity_directory, "/Client.csv"))) == 36) {
+  Clarity_Client <-
+    read_csv(paste0(clarity_directory, "/Client.csv"),
+             col_types = "nccccncnDnnnnnnnnnnnnnnnnnnnnnnTTcTn") %>%
+    filter(!PersonalID %in% c(5, 4216)) # our fake Client IDs are 5 and 4216
+} else {
+  Clarity_Client <-
+    read_csv(paste0(clarity_directory, "/Client.csv"),
+             col_types = "ncncnDnnnnnnnnnnnnnnnnnnnnnnTTcTn") %>%
+    filter(!PersonalID %in% c(5, 4216))
+}
+
+# Masking PII in the Client file (but not DOB) 
+
+if(ncol(read_csv(paste0(clarity_directory, "/Client.csv"))) == 36)
+{Clarity_Client <- Clarity_Client %>%
+  mutate(
+    # FirstName = case_when(
+    #   NameDataQuality %in% c(8, 9) ~ "DKR",
+    #   NameDataQuality == 2 ~ "Partial",
+    #   NameDataQuality == 99 |
+    #     is.na(NameDataQuality) |
+    #     FirstName == "Anonymous" ~ "Missing",!(
+    #       NameDataQuality %in% c(2, 8, 9, 99) |
+    #         is.na(NameDataQuality) |
+    #         FirstName == "Anonymous"
+    #     ) ~ "ok"
+    # ),
+    # # LastName = NULL,
+    # MiddleName = NULL,
+    # NameSuffix = NULL,
+    SSN = case_when(
+      (is.na(SSN) & !SSNDataQuality %in% c(8, 9)) |
+        is.na(SSNDataQuality) | SSNDataQuality == 99 ~ "Missing",
+      SSNDataQuality %in% c(8, 9) ~ "DKR",
+      (nchar(SSN) != 9 & SSNDataQuality != 2) |
+        substr(SSN, 1, 3) %in% c("000", "666") |
+        substr(SSN, 1, 1) == 9 |
+        substr(SSN, 4, 5) == "00" |
+        substr(SSN, 6, 9) == "0000" |
+        SSNDataQuality == 2 |
+        SSN %in% c(
+          111111111,
+          222222222,
+          333333333,
+          444444444,
+          555555555,
+          777777777,
+          888888888,
+          123456789
+        ) ~ "Invalid",
+      SSNDataQuality == 2 & nchar(SSN) != 9 ~ "Incomplete"
+    )
+  )
+
+Clarity_Client <- Clarity_Client %>%
+  mutate(SSN = case_when(
+    is.na(SSN) ~ "ok",
+    !is.na(SSN) ~ SSN
+  ))}
+
+# this overwrites the raw Client.csv file on your computer with the final Client
+# object as a security measure.
+
+if(ncol(Clarity_Client) == 33)
+{write_csv(Clarity_Client, paste0(clarity_directory, "/Client.csv"), append = FALSE)}
+
+# CurrentLivingSituation <- 
+#   read_csv(paste0(clarity_directory, "/CurrentLivingSituation.csv"),
+#             col_types = "nnnTncnnnnncTTcTc") DON'T NEED YET
+
+Clarity_Disabilities <-
+  read_csv(paste0(clarity_directory, "/Disabilities.csv"),
+           col_types = "cnnDnnnnnnnnnnTTnTn")
+
+Clarity_EmploymentEducation <-
+  read_csv(paste0(clarity_directory, "/EmploymentEducation.csv"),
+           col_types = "cnnDnnnnnnTTnTn")
+
+Clarity_Exit <-
+  read_csv(paste0(clarity_directory, "/Exit.csv"),
+           col_types = "nnnDncnnnnnnnnnnnnnnnnnnnnnnnnnDnnnnnnTTnTn")
+
+Clarity_Export <- read_csv(paste0(clarity_directory, "/Export.csv"),
+                           col_types = c("iicccccccTDDcciii"))
+
+Clarity_Project <- 
+  read_csv(paste0(clarity_directory, "/Project.csv"),
+           col_types = "nnccDDnnnnnnnnTTcTn") 
+
+Clarity_EnrollmentCoC <- 
+  read_csv(paste0(clarity_directory, "/EnrollmentCoC.csv"), 
+           col_types = "cncnnDcnTTnTn")
+
+Clarity_Enrollment <-
+  read_csv(paste0(clarity_directory, "/Enrollment.csv"),
+           col_types =
+             "nnnDcnnnlnDnnnDDDnnnncccnnDnnnncnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnTTnTn")
+
+Clarity_Funder <- 
+  read_csv(paste0(clarity_directory, "/Funder.csv"),
+           col_types = "nnnccDDTTcTn")
+
+Clarity_HealthAndDV <-
+  read_csv(paste0(clarity_directory, "/HealthAndDV.csv"),
+           col_types = "cnnDnnnnnnnDnTTnTn")
+
+Clarity_IncomeBenefits <- 
+  read_csv(paste0(clarity_directory, "/IncomeBenefits.csv"),
+           col_types = 
+             "cnnDnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnncnnnnnnncnnnnnnnnnnnnnnnnnnnncnnnnnnTTnTn")
+
+Clarity_Inventory <-
+  read_csv(paste0(clarity_directory, "/Inventory.csv"),
+           col_types = "nncnnnnnnnnnnnnDDTTcTn")
+
+Clarity_Organization <- 
+  read_csv(paste0(clarity_directory, "/Organization.csv"),
+           col_types = "ncncTTnTn")
+
+Clarity_ProjectCoC <- 
+  read_csv(paste0(clarity_directory, "/ProjectCoC.csv"),
+           col_types = "nncnccccnnTTcTn")
+
 # Compare -----------------------------------------------------------------
 
-sp_enrollment_count <- Enrollment %>%
-  left_join(Project, by = "ProjectID") %>%
+sp_enrollment_count <- SP_Enrollment %>%
+  left_join(SP_Project, by = "ProjectID") %>%
   count(ProjectName) 
 
 diff_enrollment_count <- clarity_enrollment_count %>%
@@ -171,10 +300,10 @@ clarity_example <-
     ) %>%
   select("FirstName" = 2, "LastName" = 3, "EntryDate" = 4) 
 
-sp_example <- Enrollment %>%
+sp_example <- SP_Enrollment %>%
   filter(ProjectID == 288) %>%
   select(PersonalID, EntryDate) %>%
-  left_join(Client %>% 
+  left_join(SP_Client %>% 
               select(PersonalID, FirstName, LastName), 
             by = "PersonalID") %>%
   select(-PersonalID)
@@ -187,4 +316,24 @@ duplications_sp <- sp_example %>%
 
 duplications_clarity <- clarity_example %>%
   get_dupes() %>% unique()
+
+
+# Export Comparison -------------------------------------------------------
+
+SP_Export$ExportStartDate == Clarity_Export$ExportStartDate
+SP_Export$ExportEndDate == Clarity_Export$ExportEndDate
+
+# all clients in the SP export that are NOT in the Clarity export
+missing_clients <- anti_join(SP_Client, Clarity_Client, 
+                             by = c("FirstName",
+                                    "LastName",
+                                    "SSN",
+                                    "DOB"))
+
+# Clarity can only take one CoC at a time so let's pretend that's the only problem
+bos_missing_clients <- missing_clients %>%
+  left_join(SP_EnrollmentCoC %>%
+              select(PersonalID, CoCCode), by = "PersonalID") %>%
+  filter(CoCCode == "OH-507")
+
 
